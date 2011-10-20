d193ba1b2e2dbb088f81a7e3f43dc5dfaf82a1b3
= <=>
2011-10-20 14:28:05 -0200

    lot of stuff: better templates, new config file definition, changes on the database. Now login is working and is almost opmizied

--- login_test.py
+++ login_test.py
@@ -11,10 +11,12 @@ from tornado import database
 
 from testutils import init_db, drop_db
 
+from mock import patch
+
 APP_ROOT = os.path.abspath(os.path.join(os.path.dirname(__file__), '.'))
 sys.path.append(os.path.join(APP_ROOT, '.'))
 
-parse_config_file(os.getenv("HOME") + "/dojo_to.conf")
+parse_config_file(os.getenv("HOME") + "/.dojo_to.conf")
 
 class LoginTest(AsyncHTTPTestCase):
 
@@ -24,7 +26,11 @@ class LoginTest(AsyncHTTPTestCase):
         super(LoginTest, self).setUp()
 
     def get_app(self):
-        return DojoTo(options)
+        dojo_to = DojoTo(options)
+        for h in dojo_to.handlers:
+            print h[1][0].handler_class
+        #print dojo_to.handlers
+        return dojo_to
 
     def test_access_login_and_be_redirect_to_twitter(self):
         #response = self.fetch('/login/twitter', follow_redirects=False)
@@ -36,11 +42,20 @@ class LoginTest(AsyncHTTPTestCase):
             oauth_token="31CYtN2gap7BL4t1m6o8oqQwz047VTg8rZXm9LHtOY4",
             oauth_verifier= "6bpcvonh0GWzZP4oUGyhq2BuMHt2cOe4kpuSYz7Rk",
         )
+        #application = [];
+        #TwitterHandler(application=None, request=None)
+        #patcher = patch('TwitterHandler')
+        #mocked = patcher.start()
+        #self.get_authenticated_user(self.async_callback(self._on_auth))
+        #patcher.stop()
         args = urlencode(oauth)
+
+        #handler = self.
         #response = self.fetch('/login/twitter?'+args, follow_redirects=False)
+        #self.assertEquals(302, response.code)
+
         #token cookie
         #self.assertFalse(True)
-        pass
         #oauth_token=31CYtN2gap7BL4t1m6o8oqQwz047VTg8rZXm9LHtOY4&oauth_verifier=6bpcvonh0GWzZP4oUGyhq2BuMHt2cOe4kpuSYz7Rk
 
 if __name__ == '__main__':
