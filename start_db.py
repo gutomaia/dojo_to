0453f26dd56d1e24f5183aa02389484cc2f46514
Guto Maia <guto@guto.net>
2011-10-20 16:33:08 -0200

    fix the start db script dosent import parse_config_file

--- start_db.py
+++ start_db.py
@@ -3,6 +3,8 @@
 import unittest, os, os.path, sys, urllib
 from urllib import urlencode
 
+from tornado.options import parse_config_file
+
 from testutils import init_db, drop_db
 
 APP_ROOT = os.path.abspath(os.path.join(os.path.dirname(__file__), '.'))
